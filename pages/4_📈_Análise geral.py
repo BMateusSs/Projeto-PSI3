362bab8337737719de0ed6a3da83e5ed3acd27c3
BMateusSs <bmdss0810@gmail.com>
2025-05-13 18:50:03 -0300

    Nova página

---
+++ pages/4_📈_Análise geral.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_parquet('data/processed/winemag.parquet')
+df = df[df['designation'] != 'Non-designated']
+
+df_designation_count = df.groupby('designation').size().reset_index(name='count')
+df_designation_count = df_designation_count.sort_values('count', ascending=False).head(20)
+
+max_count = df_designation_count['count'].max()
+
+fig = px.bar(
+    df_designation_count,
+    x='designation',
+    y='count',
+    title='Quantidade de descrições por `designation`',
+    labels={'designation': 'Designation', 'count': 'Quantidade'}
+)
+
+fig.update_layout(
+    xaxis=dict(tickangle=-45),
+    yaxis=dict(range=[0, max_count])
+)
+
+st.plotly_chart(fig)
+
+df = df[df['province'] != 'Non-province']
+
+df_province_count = df.groupby('province').size().reset_index(name='count')
+df_province_count = df_province_count.sort_values('count', ascending=False).head(25)
+
+max_count = df_province_count['count'].max()
+
+fig = px.bar(
+    df_province_count,
+    x='province',
+    y='count',
+    title='Quantidade de descrições por province',
+    labels={'province': 'province', 'count': 'Quantidade'}
+)
+
+fig.update_layout(
+    xaxis=dict(tickangle=-45),
+    yaxis=dict(range=[0, max_count])
+)
+
+st.plotly_chart(fig)
+
+df = df[df['variety'].notna()]
+
+df_variety_count = df.groupby('variety').size().reset_index(name='count')
+df_variety_count = df_variety_count.sort_values('count', ascending=False).head(25)
+
+max_count = df_variety_count['count'].max()
+
+fig = px.bar(
+    df_variety_count,
+    x='variety',
+    y='count',
+    title='Quantidade de descrições por variedade de vinho',
+    labels={'variety': 'Variedade', 'count': 'Quantidade'}
+)
+
+fig.update_layout(
+    xaxis=dict(tickangle=-45),
+    yaxis=dict(range=[0, max_count])
+)
+
+st.plotly_chart(fig)
