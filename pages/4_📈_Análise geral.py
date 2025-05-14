b2912bb782d2ec80db6ab5174820303320432a09
mariaclaracti <mc4413329@gmail.com>
2025-05-13 21:26:37 -0300

    scatter desc_length x points

--- pages/4_📈_Análise geral.py
+++ pages/4_📈_Análise geral.py
@@ -81,3 +81,15 @@ ax.imshow(wordcloud, interpolation='bilinear')
 ax.axis("off")
 st.pyplot(fig)
 
+df['desc_length'] = df['description'].dropna().apply(len)
+fig = px.scatter(
+    df,
+    x='desc_length',
+    y='points',
+    title='Comprimento da descrição x Pontuação',
+    labels={'desc_length': 'Comprimento da descrição', 'points': 'Pontuação'},
+    opacity=0.6,
+    trendline='ols',
+    color='points'
+)
+st.plotly_chart(fig)
