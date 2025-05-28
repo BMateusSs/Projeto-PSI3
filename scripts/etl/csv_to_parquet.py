398eb2d5c85d5052e2216629b931374864402e73
BMateusSs <bmdss0810@gmail.com>
2025-05-28 10:27:31 -0300

    Correção do dataset

--- scripts/etl/csv_to_parquet.py
+++ scripts/etl/csv_to_parquet.py
@@ -1,5 +1,5 @@
 import pandas as pd
 
-df = pd.read_csv("data/raw/combined_wine_quality.csv")
+df = pd.read_csv("data/raw/combined_wine_quality.csv", sep=';')
 
-df.to_parquet("data/processed/wine-quality.parquet", engine="pyarrow")
+df.to_parquet("data/processed/wine-quality.parquet", engine="pyarrow", index=False)
